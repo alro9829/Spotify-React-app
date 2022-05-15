b660690dadd0619dc1761ce7bdeefea8fe797ec6
alro9829 <70238044+alro9829@users.noreply.github.com>
2022-05-15 01:23:44 -0600

    main

--- src/components/dropdown.jsx
+++ src/components/dropdown.jsx
@@ -1,5 +1,23 @@
 import React, { Component } from "react";
 
 class Dropdown extends Component {}
+    data = [
+        { value: 1, name: "A" },
+        { value: 2, name: "B" },
+        { value: 3, name: "C" },
+    ];
 
+    render() {
+        return (
+        <div>
+            <select>
+            {data.map((item, idx) => (
+                <option key={idx} value={item.value}>
+                {item.name}
+                </option>
+            ))}
+            </select>
+        </div>
+        );
+    }
 export default Dropdown;
